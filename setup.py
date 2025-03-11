c264f36a33b5de40acda870181471e9f0f172cd4
denispetrovskiy92@gmail.com <Bumba1010!>
2025-03-11 07:16:34 +0300

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='powernovo2',
-    version='1.0.10',
+    version='1.0.11',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/protdb/PowerNovo2.git',
